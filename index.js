92a8f88622dc5ade6d089fddc05b3783712459da
Muhammad Tahir Raza <tahirrazagb1029@gmail.com>
2025-05-08 18:00:09 +0500

    Work Completed

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*", // Allow all origins (for testing)
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }));
+  
 app.use("/auth", userRoutes);
 
 connectDB();
